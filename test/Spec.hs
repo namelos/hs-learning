0683624865289fea53f05b78954f2e4fe75b7081
Yifan Wang <namelos@outlook.com>
2018-08-29 12:28:10 +0800

    simple functions

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,3 +10,14 @@ main = hspec $ do
   describe "add" $ do
     it "should add 1 2 equals 3" $ do
       add 1 2 `shouldBe` 3
+
+  describe "isSorted" $ do
+    it "should return True when isSorted (<) [1,2,3]" $ do
+      isSorted (<) [1,2,3] `shouldBe` True
+    
+    it "should return False when isSorted (<) [1,3,2]" $ do
+      isSorted (<) [1,3,2] `shouldBe` False
+
+  describe "fib" $ do
+    it "fib 10 should be 55" $ do
+      fib 10 `shouldBe` 55
