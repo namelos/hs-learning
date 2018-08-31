6d3aec61e657eaee41aa699e6e36baf769d3a6a6
namelos <namelos@outlook.com>
2018-08-31 22:20:48 +0800

    change directory structure add ch2

---
+++ src/Chapter2/Section4/HigherOrderFunctionExercise.hs
@@ -0,0 +1,8 @@
+module Chapter2.Section4.HigherOrderFunctionExercise where
+
+-- please define a tail call fib
+
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 1
+fib 10 = 55
---
+++ test/Chapter2/Section5/PolymorhphicFunctionSpec.hs
@@ -0,0 +1,22 @@
+module Chapter2.Section5.PolymorhphicFunctionSpec where
+
+import Chapter2.Section5.PolymorhphicFunction
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "findFirstString" $ do
+    it "should return first matched string index" $ do
+      findFirstString "beta" ["alpha", "beta", "gamma"] `shouldBe` 1
+    it "should not find the second match" $ do
+      findFirstString "beta" ["alpha", "beta", "beta"] `shouldBe` 1
+    it "should return -1 if target value could not be found" $ do
+      findFirstString "omega" ["alpha", "beta", "gamma"] `shouldBe` -1
+
+  describe "findFirst" $ do
+    it "should return first matched value index" $ do
+      findFirst 2 [1,2,3] `shouldBe` 1
+    it "should not find the second match" $ do
+      findFirst 2 [1,2,2] `shouldBe` 1
+    it "should return -1 if target value could not be found" $ do
+      findFirst 5 [1,2,3] `shouldBe` -1
