0dd4e7999d946f222391c66a4d9487dd5f6d4b56
namelos <namelos@outlook.com>
2018-10-14 23:57:35 +0800

    wording change

--- src/Chapter2/Section6/FollowingTypeToImplementation.hs
+++ src/Chapter2/Section6/FollowingTypeToImplementation.hs
@@ -1,12 +1,23 @@
 module Chapter2.Section6.FollowingTypeToImplementation where
 
--- partial1 takes a function which takes tuple
--- and return a function of one argument as its result
+
+{-
+  partial1 takes a function which takes tuple
+  and return a function of one argument as its result
+
+  First, given signature:
+    partial1 :: ((a, b) -> c) -> a -> (b -> c)
+  The result type is a function b -> c
+
+  So it takes a parameter: \b -> ...
+  We can start writing:
+    partial1 f a = \b -> ...
+  Finally, c is the return type of returned function,
+  and the only way to get type c is to call the function in parameter:
+    f (a, b)
+
+  So the final implementation would be:
+-}
 
 partial1 :: ((a, b) -> c) -> a -> (b -> c)
 partial1 f a = \b -> f (a, b)
-
--- the result type of partial1 is b -> c
--- so it takes a parameter: b \b -> ...
--- and then call f as it should: f (a, b)
--- the implementation should be \b -> f (a, b)
