d23892b1b9ec456de382559be4c54b22bc0a1468
namelos <namelos@outlook.com>
2018-10-15 22:00:49 +0800

    remove test implementation

--- src/Chapter2/Section4/HigherOrderFunctionExercise.hs
+++ src/Chapter2/Section4/HigherOrderFunctionExercise.hs
@@ -3,6 +3,4 @@ module Chapter2.Section4.HigherOrderFunctionExercise where
 -- please define a tail call fib
 
 fib :: Int -> Int
-fib 1 = 1
-fib 2 = 1
-fib 10 = 55
+fib = undefined
--- src/Chapter3/Section3/DataSharing.hs
+++ src/Chapter3/Section3/DataSharing.hs
@@ -5,7 +5,6 @@ import Chapter3.Section1.LinkedList
   Functional programming data structure is immutable.
   If we add or remove an element from a list,
   like from Cons 1 $ Cons 2 $ Cons 3 $ Cons 4 Empty
-
 -}
 
 append :: List' a -> List' a -> List' a
