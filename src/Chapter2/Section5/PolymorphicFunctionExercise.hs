ced258e5cbd6b58d7b248df7aea75441522371b2
namelos <namelos@outlook.com>
2018-10-15 23:09:11 +0800

    add polymorphic function tests

--- src/Chapter2/Section5/PolymorphicFunctionExercise.hs
+++ src/Chapter2/Section5/PolymorphicFunctionExercise.hs
@@ -1,4 +1,6 @@
 module Chapter2.Section5.PolymorphicFunctionExercise where
 
 isSorted :: (a -> a -> Bool) -> [a] -> Bool
-isSorted f as = undefined
+isSorted _ [] = True
+isSorted _ [_] = True
+isSorted p (x:y:l) = p x y && isSorted p (y:l)
